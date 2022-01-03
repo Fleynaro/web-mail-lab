fb38324d6ecaf10945651ae0ca5c26145b2e8021
Fleynaro <dariusjohnson555@gmail.com>
2022-01-04 02:08:14 +0500

    poper added

--- spa/src/main.js
+++ spa/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import { Popover } from 'bootstrap';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new Popover(popoverTriggerEl)
+})
